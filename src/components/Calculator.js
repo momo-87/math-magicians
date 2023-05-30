48f1bb7a1009ba51e92400731c256489e0b8f1d6
Momo-87 <momochristian87@gmail.com>
2023-05-30 13:22:36 +0100

    Styling the calculator design

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,33 @@
 const CalculatorDesign = () => {
-  const Input = () => (<input className="input" type="number" />);
-  const Operators = () => (
-    <div className="operators-box">
-      <button className="division" type="button">/</button>
-      <button className="division" type="button">x</button>
-      <button className="division" type="button">-</button>
-      <button className="division" type="button">+</button>
-      <button className="division" type="button">=</button>
-    </div>
-  );
-  const Numbers = () => (
-    <div className="numbers-box">
-      <button className="ac" type="button">AC</button>
-      <button className="plus-minus" type="button">+/-</button>
-      <button className="percentage" type="button">%</button>
-      <button className="seven" type="button">7</button>
-      <button className="eight" type="button">8</button>
-      <button className="nine" type="button">9</button>
-      <button className="four" type="button">4</button>
-      <button className="five" type="button">5</button>
-      <button className="six" type="button">6</button>
-      <button className="one" type="button">1</button>
-      <button className="two" type="button">2</button>
-      <button className="three" type="button">3</button>
-      <button className="zero" type="button">0</button>
-      <button className="dot" type="button">.</button>
-    </div>
+  const Input = () => (<div className="input-box"><input className="input" type="number" placeholder="0" /></div>);
+  const Buttons = () => (
+    <>
+      <div className="AC-box"><button className="AC-btn" type="button">AC</button></div>
+      <div className="plus-minus-box"><button className="plus-minus-btn" type="button">+/-</button></div>
+      <div className="percentage-box"><button className="percentage-btn" type="button">%</button></div>
+      <div className="operator-btn"><button className="division" type="button">÷</button></div>
+      <div className="number-btn"><button className="number-btn" type="button">7</button></div>
+      <div className="number-btn"><button className="number-btn" type="button">8</button></div>
+      <div className="number-btn"><button className="number-btn" type="button">9</button></div>
+      <div className="operator-btn"><button className="multiplication" type="button">x</button></div>
+      <div className="number-btn"><button className="number-btn" type="button">4</button></div>
+      <div className="number-btn"><button className="number-btn" type="button">5</button></div>
+      <div className="number-btn"><button className="number-btn" type="button">6</button></div>
+      <div className="operator-btn"><button className="soustraction" type="button">-</button></div>
+      <div className="number-btn"><button className="number-btn" type="button">1</button></div>
+      <div className="number-btn"><button className="number-btn" type="button">2</button></div>
+      <div className="number-btn"><button className="number-btn" type="button">3</button></div>
+      <div className="operator-btn"><button className="addition" type="button">+</button></div>
+      <div className="number-btn zero"><button className="zero" type="button">0</button></div>
+      <div className="dot-box"><button className="dot" type="button">.</button></div>
+      <div className="operator-btn"><button className="equal" type="button">=</button></div>
+    </>
   );
 
   return (
     <div className="wrapper">
       <Input />
-      <Operators />
-      <Numbers />
+      <Buttons />
     </div>
   );
 };
