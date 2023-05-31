f6a440855a933dce6d85170a1a9d14091dc42347
Momo-87 <momochristian87@gmail.com>
2023-05-31 18:21:10 +0100

    Removing button container

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,52 @@
-const CalculatorDesign = () => {
-  const Input = () => (<div className="input-box"><input className="input" type="number" placeholder="0" /></div>);
-  const Buttons = () => (
-    <>
-      <div className="AC-box"><button className="AC-btn" type="button">AC</button></div>
-      <div className="plus-minus-box"><button className="plus-minus-btn" type="button">+/-</button></div>
-      <div className="percentage-box"><button className="percentage-btn" type="button">%</button></div>
-      <div className="operator-btn"><button className="division" type="button">÷</button></div>
-      <div className="number-btn"><button className="number-btn" type="button">7</button></div>
-      <div className="number-btn"><button className="number-btn" type="button">8</button></div>
-      <div className="number-btn"><button className="number-btn" type="button">9</button></div>
-      <div className="operator-btn"><button className="multiplication" type="button">x</button></div>
-      <div className="number-btn"><button className="number-btn" type="button">4</button></div>
-      <div className="number-btn"><button className="number-btn" type="button">5</button></div>
-      <div className="number-btn"><button className="number-btn" type="button">6</button></div>
-      <div className="operator-btn"><button className="soustraction" type="button">-</button></div>
-      <div className="number-btn"><button className="number-btn" type="button">1</button></div>
-      <div className="number-btn"><button className="number-btn" type="button">2</button></div>
-      <div className="number-btn"><button className="number-btn" type="button">3</button></div>
-      <div className="operator-btn"><button className="addition" type="button">+</button></div>
-      <div className="number-btn zero"><button className="zero" type="button">0</button></div>
-      <div className="dot-box"><button className="dot" type="button">.</button></div>
-      <div className="operator-btn"><button className="equal" type="button">=</button></div>
-    </>
-  );
+// import { useState } from 'react';
+// import calculate from '../logic/calculate';
 
-  return (
-    <div className="wrapper">
-      <Input />
-      <Buttons />
-    </div>
-  );
-};
-export default CalculatorDesign;
+const Calculator = () => (
+  // const [obj, setObject] = useState({ total: '', next: '', operation: '' });
+
+  <div className="wrapper">
+    <Input />
+    <ACBtn name="AC" />
+    <PlusMinusBtn name="+/-" />
+    <ModuloBtn name="%" />
+    <DivisionBtn name="÷" />
+    <SevenBtn name="7" />
+    <EightBtn name="8" />
+    <NineBtn name="9" />
+    <MultiplicationBtn name="x" />
+    <FourBtn name="4" />
+    <FiveBtn name="5" />
+    <SixBtn name="6" />
+    <MinusBtn name="-" />
+    <OneBtn name="1" />
+    <TwoBtn name="2" />
+    <ThreeBtn name="3" />
+    <PlusBtn name="+" />
+    <ZeroBtn name="0" />
+    <DotBtn name="." />
+    <EqualBtn name="=" />
+  </div>
+);
+
+// onClick={calculate(obj, props.name)}
+const Input = () => (<div className="input-box"><input className="input" type="number" placeholder="0" /></div>);
+const ACBtn = () => (<button className=" btn AC-btn" type="button">AC</button>);
+const PlusMinusBtn = () => (<button className="btn plus-minus-btn" type="button">+/-</button>);
+const ModuloBtn = () => (<button className="btn percentage-btn" type="button">%</button>);
+const DivisionBtn = () => (<button className="btn division" type="button">÷</button>);
+const SevenBtn = () => (<button className="btn number-btn" type="button">7</button>);
+const EightBtn = () => (<button className="btn number-btn" type="button">8</button>);
+const NineBtn = () => (<button className="btn number-btn" type="button">9</button>);
+const MultiplicationBtn = () => (<button className="btn multiplication" type="button">x</button>);
+const FourBtn = () => (<button className="btn number-btn" type="button">4</button>);
+const FiveBtn = () => (<button className="btn number-btn" type="button">5</button>);
+const SixBtn = () => (<button className="btn number-btn" type="button">6</button>);
+const MinusBtn = () => (<button className="btn soustraction" type="button">-</button>);
+const OneBtn = () => (<button className="btn number-btn" type="button">1</button>);
+const TwoBtn = () => (<button className="btn number-btn" type="button">2</button>);
+const ThreeBtn = () => (<button className="btn number-btn" type="button">3</button>);
+const PlusBtn = () => (<button className="btn addition" type="button">+</button>);
+const ZeroBtn = () => (<button className="btn zero" type="button">0</button>);
+const DotBtn = () => (<button className="btn dot" type="button">.</button>);
+const EqualBtn = () => (<button className="btn equal" type="button">=</button>);
+export default Calculator;
