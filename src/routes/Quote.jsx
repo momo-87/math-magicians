de23e6542d13c68c40cc153f458b1ec10d4c3a8b
Momo-87 <momochristian87@gmail.com>
2023-06-07 03:34:00 +0100

    Add style to the Quote route

--- src/routes/Quote.jsx
+++ src/routes/Quote.jsx
@@ -1,8 +1,9 @@
 import DisplayQuote from 'components/DisplayQuote';
+import styles from 'styles/Quote.module.css';
 
 const Quote = () => (
-  <>
+  <div className={styles.quote}>
     <DisplayQuote />
-  </>
+  </div>
 );
 export default Quote;
